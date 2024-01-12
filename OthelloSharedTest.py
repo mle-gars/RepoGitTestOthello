f23fa197b5f0297ddb281596b1b164529c04bf84
mle-gars <maxime.legars@hotmail.fr>
2024-01-12 14:17:44 +0100

    check valid moves 1.0

--- OthelloSharedTest.py
+++ OthelloSharedTest.py
@@ -264,22 +264,17 @@ class Bot:
     def check_valid_moves(self, board, game):
        
         print("Il faut récupérer toutes les cases du tableau")
-
+        print("Vérifier quels coups sont jouables")
+        print("Et renvoyer les coordonnées")
         valid_moves = []
         for tile_index in board.board:
             move_to_check = board.is_legal_move(tile_index.x_pos, tile_index.y_pos, game.active_player)
             if move_to_check:
                 valid_moves.append([tile_index.x_pos, tile_index.y_pos])
                 print(valid_moves)
-            #return valid_moves
+            return valid_moves
 
-        print(board) # affiche mes cases de tableau
                 
-        print("Vérifier quels coups sont jouables")
-
-
-
-        print("Et renvoyer les coordonnées")
 
 # Create a new board & a new game instances
 othello_board = Board(8)
